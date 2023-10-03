d460a19d04dde9c83baab01c5c7c34595807ea49
Sakshi Soni <114993517+sakshi-sonii@users.noreply.github.com>
2023-10-03 12:42:34 +0000

    gets column name for a number as in excel

--- GetColumnName.py
+++ GetColumnName.py
@@ -1,17 +1,19 @@
 class Solution:
-    def colName (self, n):
+    def colName(self, n):
         # your code here
         string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        mapping = dict(zip(range(1,27),string))
+        mapping = dict(zip(range(1, 27), string))
         ans = ""
-        while n > 0 : 
-            rem = n%26
-            ans =  mapping.get(rem,'Z') + ans 
-            n= n//26 
+        while n > 0:
+            rem = n % 26
+            ans = mapping.get(rem, 'Z') + ans
+            n = n//26
             if rem == 0:
                 n -= 1
-            
-        return ans  
+
+        return ans
+
+
 solution = Solution()
 ans = solution.colName(40)
 print(ans)
