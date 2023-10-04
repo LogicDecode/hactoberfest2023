812dca7026703f188fb56207517398079ad0e81a
Nikhil Patil <87567207+Nikhil18Patil@users.noreply.github.com>
2023-10-04 23:23:51 +0530

    Update simpleCalculator.py
    I added an option for the user to quit the calculator by entering '5'.
    I added input validation for division by zero to prevent runtime errors.
    I formatted the output messages for better readability.
    I used f-strings to format the output with variables.
    I improved the user prompt for the next calculation.

--- Python/simpleCalculator.py
+++ Python/simpleCalculator.py
@@ -1,54 +1,53 @@
-# Program make a simple calculator
-
-# This function adds two numbers
+# Define functions for the operations
 def add(x, y):
     return x + y
 
-# This function subtracts two numbers
 def subtract(x, y):
     return x - y
 
-# This function multiplies two numbers
 def multiply(x, y):
     return x * y
 
-# This function divides two numbers
 def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
     return x / y
 
-
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-
+# Display the menu and get user input
 while True:
-    # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    print("\nSelect operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Quit")
 
-    # check if choice is one of the four options
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    if choice == '5':
+        print("Calculator is exiting. Goodbye!")
+        break
+
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
         if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+            print(f"{num1} + {num2} = {add(num1, num2)}")
 
         elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            print(f"{num1} - {num2} = {subtract(num1, num2)}")
 
         elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+            print(f"{num1} * {num2} = {multiply(num1, num2)}")
 
         elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-        
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
-    
+            result = divide(num1, num2)
+            if isinstance(result, str):
+                print(result)
+            else:
+                print(f"{num1} / {num2} = {result}")
+
     else:
         print("Invalid Input")
+
