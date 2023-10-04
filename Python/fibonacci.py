6094c0aebcdca2e9d7e4db3a219356d080cab142
Nikhil Patil <87567207+Nikhil18Patil@users.noreply.github.com>
2023-10-04 23:19:24 +0530

    Update fibonacci.py
    This code calculates the Fibonacci sequence using a loop and two variables (a and b) to keep track of the current and next Fibonacci numbers. """It is more efficient than the recursive approach for larger values of terms."""

--- Python/fibonacci.py
+++ Python/fibonacci.py
@@ -7,21 +7,21 @@
 
 # Python program to display the Fibonacci sequence
 
-def recur_fibo(n):
-   if n <= 1:
-       return n
-   else:
-       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+#It is more efficient than the recursive approach for larger values of nterms.
 
 nterms = 10
 
-# check if the number of terms is valid
+# Check if the number of terms is valid
 if nterms <= 0:
-   print("Plese enter a positive integer")
+   print("Please enter a positive integer")
 else:
    print("Fibonacci sequence:")
+   a, b = 0, 1  # Initialize the first two Fibonacci numbers
    for i in range(nterms):
-       print(recur_fibo(i))
+       print(a)
+       a, b = b, a + b  # Update a and b to calculate the next Fibonacci number
+
        
        
        
