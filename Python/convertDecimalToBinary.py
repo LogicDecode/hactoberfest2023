17e0f021a557c1c7af879ec69a8b041844305952
Nikhil Patil <87567207+Nikhil18Patil@users.noreply.github.com>
2023-10-04 23:28:24 +0530

    Update convertDecimalToBinary.py
    These changes make the code more self-explanatory and adhere to good coding practices.

--- Python/convertDecimalToBinary.py
+++ Python/convertDecimalToBinary.py
@@ -1,11 +1,20 @@
-# Function to print binary number using recursion
-def convertToBinary(n):
-   if n > 1:
-       convertToBinary(n//2)
-   print(n % 2,end = '')
+def decimal_to_binary_recursive(n):
+    """
+    Convert a decimal number to binary using recursion.
 
-# decimal number
+    Args:
+        n (int): The decimal number to be converted.
+
+    Returns:
+        None: Prints the binary representation.
+    """
+    if n > 1:
+        decimal_to_binary_recursive(n // 2)
+    print(n % 2, end='')
+
+# Decimal number
 dec = 34
 
-convertToBinary(dec)
+decimal_to_binary_recursive(dec)
 print()
+
